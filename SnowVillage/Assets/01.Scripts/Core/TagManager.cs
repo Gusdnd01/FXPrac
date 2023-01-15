4417b3ce6a8460902464c92d257bb5b0e168a9bb
Gusdnd01 <cestop42@naver.com>
2023-01-16 00:44:36 +0900

    파일 정리

--- SnowVillage/Assets/01.Scripts/Core/TagManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class TagManager //써야할 태그 등을 쓰기 위한 코드
-{
-    public static string TAG_PLAYER = "Player";
-    public static string TAG_ENEMY = "Enemy";
-}
